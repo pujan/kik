0d0abf229044dcc945147ddc9b1b6a1cc08eab77
Łukasz A. Pelc <lukasz.pelc.81@gmail.com>
2022-10-07 08:23:45 +0200

    Hack for GitHub build

--- ttt/lang.py
+++ ttt/lang.py
@@ -7,5 +7,12 @@ from babel import Locale
 LANGS_PATH = Path.cwd() / 'locale'
 
 i18n.load_path.append(str(LANGS_PATH.resolve()))
-locale = Locale.parse(os.environ.get('LANG', 'en_US'))
+
+# HACK GitHub set env LANG = 'C' - crash tests
+lang = os.environ.get('LANG', 'en_US')
+
+if lang.lower() == 'c':
+    lang = 'en_US'
+
+locale = Locale.parse(lang)
 i18n.set('locale', locale.language)
